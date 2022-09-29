0a168592128ff21998adcb52e540e66319b15cf3
lebedevich-es <lebedevich.es@gmail.com>
2022-09-29 09:40:58 +0200

    Modified int to double

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        if (num > 0) {
-            System.out.println("Positive number");
+        double num = sc.nextDouble();
+        if (num >= 0.0) {
+            System.out.println(num + " is a positive number");
         } else
-            System.out.println("Negative number");
+            System.out.println(num + " is a negative number");
     }
 }
